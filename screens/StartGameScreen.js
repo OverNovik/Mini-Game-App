face97bcde9972cf7830825c7ca6939af60a7b26
Aliaxander Novikov <nerevarin11@gmail.com>
2022-08-12 17:01:19 +0300

    add PrimaryButton component, StartGameScreen field

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { Button, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  )
+}
+
+export default StartGameScreen;
